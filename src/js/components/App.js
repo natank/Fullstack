3467c7287b2b680b147afcb55e5720cfc0f8e541
Nati <natan.kamusher@gmail.com>
2020-09-07 17:25:11 +0300

    saved for remote

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,31 +1,32 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../../styles/components/app.scss';
 import Nav from './Nav'
 import About from './About'
 import Shop from './Shop'
-
+import ItemDetail from './ItemDetail'
 
 const App = () => {
-    return (
-        <div className="App">
-          <Router>
-            <Nav />
-            <Switch>
-              <Route path='/' exact component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/shop" component={Shop} />
-            </Switch>
-          </Router>
+  return (
+    <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/shop/:id" component={ItemDetail} />
+        </Switch>
+      </Router>
 
-        </div>
-    )
-  }
+    </div>
+  )
+}
 
-const Home = ()=>{
-  return(
-  <div>
-    <h1>Home Page</h1>
-  </div>)
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>)
 }
 export default App
--- src/js/components/Shop.js
+++ src/js/components/Shop.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../styles/components/app.scss'
-const Shop = ()=>{
-    return(
+import { Link } from 'react-router-dom'
+const Shop = () => {
+    useEffect(() => {
+        fetchItems()
+    }, [])
+    const [items, setItems] = useState([]);
+    const fetchItems = async () => {
+        let data = [{
+            name: "item-1",
+            description: "item1 description",
+        }, {
+            name: "item-2",
+            description: "item2 description",
+        }, {
+            name: "item-3",
+            description: "item3 description",
+        }, {
+            name: "item-4",
+            description: "item4 description",
+        }, {
+            name: "item-5",
+            description: "item5 description",
+        }, {
+            name: "item-6",
+            description: "item6 description",
+        }, {
+            name: "item-7",
+            description: "item7 description",
+        }]
+        setItems(data)
+    }
+    return (
         <div className="App">
-            <h1>Shop Page</h1>
+            {items.map((item, index) => (
+                <h1 key={index}>
+                    <Link to={`/shop/${index}`}>{item.name}</Link>
+                </h1>
+            ))}
         </div>
 
-        )
+    )
 }
 
 export default Shop;
