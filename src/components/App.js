a96c7448f705383fa679ca38eac84d66197e6d39
Nati <natan.kamusher@gmail.com>
2020-11-01 17:23:05 +0200

    added plain css styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
 import React from "react";
 import {ThemeProvider} from '@material-ui/styles';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Header from '../components/ui/Header'
-import theme from './ui/Theme'
-const App = () => {
-    return (
-      <BrowserRouter>
-      <ThemeProvider theme={theme}>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={()=><div>Home</div>}/>
-            <Route exact path="/services" component={()=><div>Services</div>}/>
-            <Route exact path="/customsoftware" component={()=><div>Custom Software</div>}/>
-            <Route exact path="/mobileapps" component={()=><div>Mobile Apps</div>}/>
-            <Route exact path="/websites" component={()=><div>Websites</div>}/>
-            <Route exact path="/revolution" component={()=><div>Revolution</div>}/>
-            <Route exact path="/about" component={()=><div>About Us</div>}/>
-            <Route exact path="/contact" component={()=><div>Contact Us</div>}/>
-            <Route exact path="/estimate" component={()=><div>Estimate</div>}/>
-          </Switch>
-      </ThemeProvider>
-      </BrowserRouter>
-    )
-  }
+import {Button} from '@material-ui/core'
+import './PlainCssButton.css';
 
+import { StylesProvider } from '@material-ui/core/styles';
+
+  {/* Your component tree.
+  Now, you can override Material-UI's styles. */}
+  
+  
+const App = () => {
+  return(
+  <StylesProvider injectFirst>
+    <Btn />
+  </StylesProvider>)
+  }
+  
+const Btn = function(){
+
+ return (
+      <Button className="button" variant="contained" color="primary">
+      Hello World
+    </Button>
+    )
+ }
 export default App
