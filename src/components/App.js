eb98669c97f75ebe97b4506c439d898774c56dbc
Nati <natan.kamusher@gmail.com>
2020-11-02 07:47:25 +0200

    Changed hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Button} from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import './PlainCssButton.css';
 
 import { StylesProvider, makeStyles } from '@material-ui/core/styles';
@@ -8,28 +8,28 @@ import { StylesProvider, makeStyles } from '@material-ui/core/styles';
 
 
 const App = () => {
-  return(
-  <StylesProvider injectFirst>
-    <PlainCSSBtn />
-    <HookBtn/>
-  </StylesProvider>
+  return (
+    <StylesProvider injectFirst>
+      <PlainCSSBtn />
+      <HookBtn />
+    </StylesProvider>
   )
 }
 
-  
 
-const PlainCSSBtn = function(){
 
- return (
-      <Button className="button" variant="contained" color="primary">
+const PlainCSSBtn = function () {
+
+  return (
+    <Button className="button" variant="contained" color="primary">
       Hello Plain
     </Button>
-    )
- }
+  )
+}
 
 
 
- const useStyles = makeStyles({
+const useStyles = makeStyles({
   root: {
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
@@ -37,13 +37,16 @@ const PlainCSSBtn = function(){
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
     height: 48,
-    padding: '0 30px'
+    padding: '0 30px',
+    '&:hover': {
+      background: 'red'
+    }
   }
 }
 )
 
- function HookBtn(){
-   const classes = useStyles();
-   return <Button className={classes.root}>Hello Hook</Button>
- }
+function HookBtn() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Hello Hook</Button>
+}
 export default App
