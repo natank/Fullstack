737ecbecc616d7d1a6a886a2c8906af685fcf8b2
Nati <natan.kamusher@gmail.com>
2020-11-02 12:56:57 +0200

    added hooks

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from '@material-ui/core'
 import './PlainCssButton.css';
 
-import { StylesProvider, makeStyles } from '@material-ui/core/styles';
+import { StylesProvider, makeStyles, styled } from '@material-ui/core/styles';
 
 
 
@@ -12,6 +12,7 @@ const App = () => {
     <StylesProvider injectFirst>
       <PlainCSSBtn />
       <HookBtn />
+      <StyledComponents />
     </StylesProvider>
   )
 }
@@ -48,5 +49,20 @@ const useStyles = makeStyles({
 function HookBtn() {
   const classes = useStyles();
   return <Button className={classes.root}>Hello Hook</Button>
+}
+
+
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 5,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+})
+
+function StyledComponents() {
+  return <MyButton>Styled Components</MyButton>
 }
 export default App
